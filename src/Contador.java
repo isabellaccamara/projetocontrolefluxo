2c9dda8e449bdaaa22e54c15f13fe91564346e9f
isabellaccamara <bell.ah@hotmail.com>
2023-10-17 09:38:21 -0300

    Controle de Fluxo - Desafio

---
+++ src/Contador.java
@@ -0,0 +1,33 @@
+
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
+public class Contador {
+    public static void main(String[] args) {
+        System.out.println("Idade:");
+        int parametroUm = 8; 
+        System.out.println("Experiência:");
+        int parametroDois = 20; 
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
